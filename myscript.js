edeaa0319e7920ddce3871cdd96ad3263223a5dc
iwnelion <anname17@hotmail.com>
2024-06-29 00:42:18 +0300

    update js

--- myscript.js
+++ myscript.js
@@ -1,41 +1,44 @@
+let humanScore=0;
+let computerScore=0;
+
+function getComputerChoice(){
+    let choices="rock paper scissors";
+    let index=Math.floor(Math.random()*3);
+    return choices[index];
+}
+
 function playRound(humanChoice, computerChoice){
-    if(humanChoice>computerChoice){
-        console.log("you win!")
+    humanChoice=prompt("what's your option?").toLocaleLowerCase();
+    computerChoice=getComputerChoice();
+    console.log(`you: ${humanChoice}`);
+    console.log(`computer: ${computerChoice}`);
+
+    if(humanChoice===computerChoice){
+        console.log("tie");
+    }
+    else if(humanChoice==="rock" && computerChoice==="scissors") || 
+    (humanChoice==="paper" && computerChoice==="rock") || 
+    (humanChoice==="scissors" && computerChoice==="paper"){
+        console.log("you win");
         humanScore++;
     }
-    else if(humanChoice<computerChoice){
-        console.log("computer wins!")
+    else{
+        console.log("computer wins");
         computerScore++;
     }
 }
+
 function displayScore(){
     console.log("score:");
         console.log("you: "+humanScore);
         console.log("computer: "+computerScore);
 }
 
-// let ch1="rock";     //1
-// let ch2="paper";    //2
-// let ch3="scissors"; //3
+// console.log(getHumanChoice());
+// console.log(getComputerChoice());
 
-let humanScore=0;
-let computerScore=0;
+// const humanSelection=getHumanChoice();
+// const computerSelection=getComputerChoice();
 
-function getComputerChoice(comp){
-    let comp=Math.random();
-    return comp;
-}
-
-function getHumanChoice(player){
-    let player=prompt("what's your option?").toLowerCase();
-    return player;
-}
-
-console.log(getHumanChoice());
-console.log(getComputerChoice());
-
-const humanSelection=getHumanChoice();
-const computerSelection=getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playRound();
 displayScore();
